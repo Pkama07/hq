5ec543fc7b349900cf3ac8c079b71ecb0cab9a10
Pkama07 <pradyun774@gmail.com>
2025-07-03 12:57:42 -0400

    replaced '

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function Home() {
 	return (
 		<div className="h-screen w-screen bg-white text-black p-5">
 			<p>
-				Hi, I'm Pradyun. I study computer science and math at Purdue. I
-				currently work at Palantir in NYC and I'm planning on working at a
+				Hi, I&apos;m Pradyun. I study computer science and math at Purdue. I
+				currently work at Palantir in NYC and I&apos;m planning on working at a
 				startup in SF this fall.
 			</p>
 			<div className="mt-5">
-				Here's a list of the projects I've built:
+				Here&apos;s a list of the projects I&apos;ve built:
 				<ul className="list-disc list-inside">
 					<li>
 						<ProjectLink
